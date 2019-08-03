6d80087c7720a82848392313f9692d911cd4d985
Oleg Urzhumtsev <darikcr@gmail.com>
2019-08-03 17:48:05 +0300

    Server backend

--- srv/server.js
+++ srv/server.js
@@ -4,6 +4,7 @@ const wss = new WebSocket.Server({ port: 3030 });
 
 wss.on('connection', ws => {
   ws.on('message', data => {
+    console.log(data);
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
@@ -12,7 +13,9 @@ wss.on('connection', ws => {
   });
   setInterval(() => {
     wss.clients.forEach(client => {
+      console.log('sending!');
       if (client !== ws && client.readyState === WebSocket.OPEN) {
+        console.log('ura!');
         client.send(JSON.stringify({ event: 'button', id: '1' }));
       }
     });  
