c4c2d33b36edd26a3e68ed6308cac3a92f8d53eb
Netbug_PI <oleg@pi>
2019-08-05 17:14:54 +0000

    Adapted GPIO bindings for buttons and sensors

--- srv/fiddle.js
+++ srv/fiddle.js
@@ -5,27 +5,39 @@ const readline = require('readline');
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
+console.log('Waiting for clients...');
 const wss = new WebSocket.Server({ port: 3030 });
 
 const m1coeff = 255;
 const m2coeff = 255;
 
 const machine1 = new Gpio(20, {mode: Gpio.OUTPUT});
-const machine2 =  new Gpio(26, {mode: Gpio.OUTPUT});
+const machine2 =  new Gpio(21, {mode: Gpio.OUTPUT});
 
 const m1en = true; //false;
 const m2en = true; //false;
 
-const button1 = new Gpio(4, {
+const button1 = new Gpio(22, {
   mode: Gpio.INPUT,
   pullUpDown: Gpio.PUD_DOWN,
-  edge: Gpio.RISING_EDGE, // FALLING_EGDE, EITHER_EDGE
+  edge: Gpio.FALLING_EDGE, // FALLING_EGDE, EITHER_EDGE
 });
-const button2 = new Gpio(5, {
+const button2 = new Gpio(27, {
   mode: Gpio.INPUT,
   pullUpDown: Gpio.PUD_DOWN,
   edge: Gpio.RISING_EDGE,
 }); // */
+const endstop1 = new Gpio(19, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_DOWN,
+  edge: Gpio.RISING_EDGE,
+});
+const endstop2 = new Gpio(26, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_DOWN,
+  edge: Gpio.RISING_EDGE,
+}); // */
+
 
 wss.on('connection', ws => {
   console.log('Set up connection!');
@@ -38,22 +50,35 @@ wss.on('connection', ws => {
     });  
   };
 
-  const btn1 = (clients) => {
-    notifyClients(clients, { event: 'button', id: '1' });
+  const btn1 = (lvl) => {
+    console.log('Button 1 pressed');
+    notifyClients(wss.clients, { event: 'button', id: '1' });
     if (m1en) machine1.pwmWrite(m1coeff);
   };
 
-  const btn2 = (clients) => {
-    notifyClients(clients, { event: 'button', id: '2' });
+  const btn2 = (lvl) => {
+    console.log('Button 2 pressed');
+    notifyClients(wss.clients, { event: 'button', id: '2' });
     if (m2en) machine2.pwmWrite(m2coeff);
   };
 
-  button1.on('interrupt', (level) => {
-    if (level === 0) log.error('WTF? Interrupt on falling edge?');
-    btn1();
-  });
+  const es1 = (lvl) => {
+    console.log('Endstop 1 triggered');
+    notifyClients(wss.clients, { event: 'button', id: '1' });
+    if (m1en) machine1.pwmWrite(m1coeff);
+  };
 
+  const es2 = (lvl) => {
+    console.log('Endstop 2 triggered!');
+    console.log(lvl);
+    notifyClients(wss.clients, { event: 'button', id: '2' });
+    if (m2en) machine2.pwmWrite(m2coeff);
+  };
+
+  button1.on('interrupt', btn1);
   button2.on('interrupt', btn2); // */
+  endstop1.on('interrupt', es1);
+  endstop2.on('interrupt', es2);
 
   process.stdin.on('keypress', function (chunk, key) {
     //console.log('Get Chunk: ' + chunk + '\n');
@@ -62,13 +87,13 @@ wss.on('connection', ws => {
     if (key && key.name == 'q') process.exit(0);
   });
 
-  ws.on('message', data => {
+  /* ws.on('message', data => {
     wss.clients.forEach(client => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
     });
-  });
+  }); */
 
 
   ws.on('message', data => {  // Sends data from single client to all others
