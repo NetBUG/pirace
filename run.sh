9c026ddcf26f66f787f09f840ff43279f9bcb908
Netbug_PI <oleg@pi>
2019-08-07 13:48:15 +0000

    Unified entry point

---
+++ run.sh
@@ -0,0 +1,7 @@
+git pull
+export NVM_DIR="$HOME/.config"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm use lts/dubnium
+sudo `which node` srv/server.js &
+cd webapp && npm start
