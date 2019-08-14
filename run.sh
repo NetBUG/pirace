cc8fdc2a37ba3193b1e0313d01f1cbbc0cc0e3c0
Netbug_PI <oleg@pi>
2019-08-14 09:48:26 +0000

    Update run scripts

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ export NVM_DIR="$HOME/.config"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm use lts/dubnium
 sudo `which node` srv/server.js &
-cd webapp && npm start
+cd webapp && npm run build
+serve -s build &
+x-www-browser http://localhost:5000
